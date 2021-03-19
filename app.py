8b9c51abd5507f883e7a29cb6eb70299ee84aed0
ArkZani <60407722+ArkZani@users.noreply.github.com>
2021-03-19 12:56:17 -0700

    Update app.py
    Removed Index

--- app.py
+++ app.py
@@ -49,9 +49,6 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/index", methods=["GET", "POST"])
-def picture():
-    return render_template("index.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
